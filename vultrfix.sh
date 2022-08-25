2b9bd88cf4d7bc23c05edcdf638e7d67010131ec
ngocsuo <hoangvanngoc37@gmail.com>
2022-08-25 23:19:54 +0700

    Update vultrfix.sh

--- vultrfix.sh
+++ vultrfix.sh
@@ -112,8 +112,7 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
-echo "How many proxy do you want to create? Max 3000"
-read COUNT
+COUNT = 350
 
 FIRST_PORT=10000
 LAST_PORT=$(($FIRST_PORT + $COUNT))
